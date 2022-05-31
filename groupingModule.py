6120d92fc568bb82af6b265c2587a585c3e2cd3a
JohanBrun <johannicbrun@gmail.com>
2022-05-31 16:45:35 +0200

    Rearrange grouping

--- groupingModule.py
+++ groupingModule.py
@@ -1,30 +1,106 @@
-from random import choice, uniform
+import math
+import random
 from numpy.random import default_rng
 rng = default_rng()
 from localTypes import Direction
 
-class GroupingModule:
-    def __init__(self) -> None:
-        composition = SectionGroup(3)
-    
-class BaseGroup:
-    index = 0
+class SectionGroup():
+    def __init__(self, numPhrases: int) -> None:
+        assert numPhrases > 1
+        self.phrases: list[PhraseGroup] = []
+        for i in range(numPhrases):
+            self.phrases.append(PhraseGroup(2))
 
-    def __init__(self, dir: Direction, meanVA: tuple[float, float]) -> None:
+    def flatten(self):
+        degrees = []
+        octaves = []
+        durations = []
+        valence = []
+        arousal = []
+        for phraseGroup in self.phrases:
+            for phraseSubGroup in phraseGroup.groups:
+                for baseGroup in phraseSubGroup.groups:
+                    degrees += baseGroup.degrees
+                    octaves += baseGroup.octaves
+                    durations += baseGroup.durations
+                    valence.append(baseGroup.valence)
+                    arousal.append(baseGroup.arousal)
+        return degrees, octaves, durations, valence, arousal
+
+    def groupDescent(self, baseGroupMethod):
+        for phraseGroup in self.phrases:
+            for phraseSubGroup in phraseGroup.groups:
+                for baseGroup in phraseSubGroup.groups:
+                    baseGroupMethod(baseGroup)
+
+class PhraseGroup:
+    def __init__(self, numGroups: int) -> None:
+        assert numGroups > 1
+        self.numGroups = numGroups
+        self.numBaseGroups = numGroups
+        self.groups: list[PhraseSubGroup] = []
+        contour = self.buildContour()
+        meanVs, meanAs = self.generateMeanVAs()
+        for i in range(self.numGroups - 1):
+            self.groups.append(PhraseSubGroup(self.numBaseGroups, (meanVs[i], meanAs[i]), contour[i]))
+        self.groups.append(EndingPhrase(self.numBaseGroups, (meanVs[-1], meanAs[-1]), contour[-1]))
+
+    def generateMeanVAs(self) -> tuple[list[float], list[float]]:
+        meanValence, meanArousal = rng.uniform(0, 10), rng.uniform(-10, 10)
+        return (
+            rng.normal(meanValence, 0.1, self.numGroups), rng.normal(meanArousal, 0.1, self.numGroups)
+        )
+
+    def buildContour(self):
+        contour = []
+        availableDirections = []
+        for i in range(math.ceil((self.numGroups * self.numBaseGroups) / 2)):
+            availableDirections += [Direction.ASCENDING, Direction.DESCENDING]
+        for i in range(self.numGroups):
+            subPhraseContour = []
+            for j in range(self.numBaseGroups):
+                subPhraseContour += [availableDirections.pop(random.randrange(0, len(availableDirections)))]
+            contour.append(subPhraseContour)
+        return contour
+
+class PhraseSubGroup:
+    def __init__(self, numGroups: int, avgVA: tuple[float, float], contour: list[Direction]) -> None:
+        assert numGroups > 1
+        self.numGroups = numGroups
+        self.contour = contour
+        self.groups: list[BaseGroup] = []
+        self.valenceMeans, self.arousalMeans = self.generateVAMeans(*avgVA)
+        self.diffV = rng.uniform(-2, 2)
+        for i in range(self.numGroups):
+            self.groups.append(BaseGroup(self.contour[i], (self.valenceMeans[i], self.arousalMeans[i]), self.diffV))
+
+    def generateVAMeans(self, avgValence: float, avgArousal: float):
+        return rng.normal(avgValence, 0.1, self.numGroups), rng.normal(avgArousal, 0.1, self.numGroups)
+
+class EndingPhrase(PhraseSubGroup):
+    def __init__(self, numGroups: int, avgVA: tuple[float, float], contour) -> None:
+        super().__init__(numGroups, avgVA, contour)
+        self.groups: list[BaseGroup] = []
+        for i in range(self.numGroups - 1):
+            self.groups.append(BaseGroup(self.contour[i], (self.valenceMeans[i], self.arousalMeans[i]), self.diffV))
+        self.groups.append(EndingGroup(self.contour[i], (self.valenceMeans[i], self.arousalMeans[i]), 0))
+
+    def generateVAMeans(self, avgValence: float, avgArousal: float):
+        valenceMeans = rng.normal(avgValence, 0.1, self.numGroups)
+        arousalMeans = []
+        for i in range(self.numGroups):
+            arousalMeans.append(avgArousal - 10 * (i))
+        return valenceMeans, arousalMeans
+
+class BaseGroup:
+    def __init__(self, dir: Direction, meanVA: tuple[float, float], diffV: float) -> None:
         self.dir = dir
         self.degrees = []
         self.octaves = []
         self.chords = []
         self.availableDegrees = []
         self.valence, self.arousal = meanVA
-        self.numUnits, self.numBeats, self.durations = self.generateRythm(meanVA[1], uniform(-2,2))
-        self.index += 1
-
-    def generateVA(self, meanVA: tuple[int, int]):
-        meanValence, meanArousal = meanVA
-        valence = rng.laplace(meanValence, 0.1, 4)
-        arousal = rng.laplace(meanArousal, 0.1, 4)
-        return valence, arousal
+        self.numUnits, self.numBeats, self.durations = self.generateRythm(meanVA[1], diffV)
 
     def generateRythm(self, arousal: float, diff: float):
         numUnits = 0
@@ -53,63 +129,7 @@ class BaseGroup:
             durations.insert(0, durations[0])
         return numUnits, numBeats, durations
 
-class PhraseGroup:
-    index = 0
-
-    def __init__(self, numGroups: int, avgVA: tuple[float, float]) -> None:
-        assert numGroups > 1
-        self.numGroups = numGroups
-        self.contour = self.buildContour()
-        self.groups: list[BaseGroup] = []
-        self.valenceMeans, self.arousalMeans = self.generateVAMeans(avgVA)
-        for dir, meanValence, meanAoursal in zip(self.contour, self.valenceMeans, self.arousalMeans):
-            self.groups.append(BaseGroup(dir, (meanValence, meanAoursal)))
-        self.index += 1
-
-    def generateVAMeans(self, avgVA):
-        avgValence, avgArousal = avgVA
-        valenceMeans = rng.laplace(avgValence, 0.1, self.numGroups)
-        arousalMeans = []
-        for i in range(self.numGroups):
-            arousalMeans.append(avgArousal + i / 4)
-        return valenceMeans, arousalMeans
-
-    def buildContour(self):
-        contour = []
-        for i in range(self.numGroups):
-            contour.append(choice([Direction.ASCENDING, Direction.DESCENDING]))
-        return contour
-
-class SectionGroup:
-    def __init__(self, numGroups: int) -> None:
-        assert numGroups > 1
-        self.numGroups = numGroups
-        self.groups: list[PhraseGroup] = []
-        avgV, avgA = self.generateAvgVA()
-        for avgV, avgA in zip(avgV, avgA):
-            print(avgV, avgA)
-            self.groups.append(PhraseGroup(self.numGroups, (avgV, avgA)))
-
-    def flatten(self):
-        degrees = []
-        octaves = []
-        durations = []
-        valence = []
-        arousal = []
-        for phraseGroup in self.groups:
-            for baseGroup in phraseGroup.groups:
-                degrees += baseGroup.degrees
-                octaves += baseGroup.octaves
-                durations += baseGroup.durations
-                valence.append(baseGroup.valence)
-                arousal.append(baseGroup.arousal)
-        return degrees, octaves, durations, valence, arousal
-
-    def groupDescent(self, baseGroupMethod):
-        for phraseGroup in self.groups:
-            for baseGroup in phraseGroup.groups:
-                baseGroupMethod(baseGroup)
-
-    def generateAvgVA(self) -> tuple[list[float], list[float]]:
-        meanValence, meanArousal = uniform(2, 8), uniform(-8, 8)
-        return rng.laplace(meanValence, 0.1, self.numGroups), rng.laplace(meanArousal, 0.1, self.numGroups)
+class EndingGroup(BaseGroup):
+    def __init__(self, dir: Direction, meanVA: tuple[float, float], diffV: float) -> None:
+        super().__init__(dir, meanVA, diffV)
+        
